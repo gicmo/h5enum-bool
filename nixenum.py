cda8ac86a963c23aa4d6b2892f29ef6b1f2a4553
Christian Kellner <christian@kellner.me>
2016-10-21 15:46:47 +0200

    nixenum.py: print hdf5 version

--- nixenum.py
+++ nixenum.py
@@ -2,7 +2,8 @@
 from __future__ import print_function
 import h5py
 
-print("H5 version: " + str(h5py.__version__))
+print("H5PY version: " + str(h5py.__version__))
+print("HDF5 version: " + str(h5py.version.api_version))
 
 fd = h5py.File("nixenum.h5")
 p = fd["metadata/s/properties/boolProperty"]
