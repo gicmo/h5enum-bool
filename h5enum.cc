7301d196a0a2c8ec651921d792963549c9eaeedc
Christian Kellner <christian@kellner.me>
2016-10-21 11:16:28 +0200

    enum <-> int conversion

--- h5enum.cc
+++ h5enum.cc
@@ -2,6 +2,171 @@
 #include <hdf5.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct conv_enum_t {
+  size_t src_size;
+  size_t dst_size;
+} conv_enum_t;
+
+
+static void
+enum_int_converter_init (hid_t src,
+			 hid_t dst,
+			 H5T_cdata_t *cdata,
+			 int forward)
+{
+  conv_enum_t *priv = (conv_enum_t *) malloc(sizeof(conv_enum_t));
+  priv->src_size = H5Tget_size(src);
+  priv->dst_size = H5Tget_size(dst);
+
+  cdata->need_bkg = H5T_BKG_NO;
+  cdata->priv = priv;
+}
+
+static void
+enum_int_converter_free(H5T_cdata_t *cdata) {
+  conv_enum_t *info = (conv_enum_t *) cdata->priv;
+  free(info);
+  cdata->priv = NULL;
+}
+
+static int enum_int_converter_conv(hid_t src,
+				   hid_t dst,
+				   H5T_cdata_t *cdata,
+				   size_t nl,
+				   size_t buf_stride,
+				   size_t bkg_stride,
+				   void *buf_i,
+				   void *bkg_i,
+				   hid_t dxpl,
+				   int forward) {
+
+  conv_enum_t *info = (conv_enum_t *) cdata->priv;
+  hid_t supertype;
+  size_t nalloc;
+
+  char *cbuf = NULL;
+  char *buf = (char *) buf_i;
+  
+  bool identical;
+  if (forward) {
+    supertype = H5Tget_super(src);
+    identical = H5Tequal(supertype, dst);
+  } else {
+    supertype = H5Tget_super(dst);
+    identical = H5Tequal(supertype, src);
+  }
+
+  if (identical) {
+    return 0;
+  }
+
+  if (buf_stride == 0) {
+    if (forward) {
+      H5Tconvert(supertype, dst, nl, buf, NULL, dxpl);
+    } else {
+      H5Tconvert(src, supertype, nl, buf, NULL, dxpl);
+    }
+  } else {
+    if (info->src_size > info->dst_size) {
+      nalloc = info->src_size*nl;
+    } else {
+      nalloc = info->dst_size*nl;
+    }
+
+    cbuf = (char *) malloc(nalloc);
+    for (int i = 0; i < nl; i++) {
+      memcpy(cbuf + (i*info->src_size),
+	     buf + (i*buf_stride),
+	     info->src_size);
+    }
+
+    if (forward) {
+      H5Tconvert(supertype, dst, nl, cbuf, NULL, dxpl);
+    } else {
+      H5Tconvert(src, supertype, nl, cbuf, NULL, dxpl);
+    }
+
+
+     for (int i = 0; i < nl; i++) {
+       memcpy(buf + (i*buf_stride),
+	      cbuf + (i*info->dst_size),
+	      info->dst_size);
+     }
+  } // buf_stride == 0
+
+  free(cbuf);
+  cbuf = NULL;
+  if (supertype > 0) {
+    H5Tclose(supertype);
+  }
+
+  return 0;
+}
+
+static herr_t enum_int_converter(hid_t src,
+				 hid_t dst,
+				 H5T_cdata_t *cdata,
+                    size_t nl,
+				 size_t buf_stride,
+				 size_t bkg_stride,
+				 void *buf_i,
+                               void *bkg_i,
+				 hid_t dxpl,
+				 int forward) {
+  //Direction ("forward"): 1 = enum to int, 0 = int to enum
+  static const char *direction[2] = {
+    "int to enum",
+    "enum to int"
+  };
+  
+  fprintf(stderr, "enum <-> int (%s) [%d]\n", direction[forward], cdata->command);
+
+  switch (cdata->command) {
+
+  case H5T_CONV_INIT:
+    enum_int_converter_init(src, dst, cdata, forward);
+    break;
+
+  case H5T_CONV_FREE:
+    enum_int_converter_free(cdata);
+    break;
+  case H5T_CONV_CONV:
+    return enum_int_converter_conv(src, dst, cdata, nl, buf_stride,
+				   bkg_stride, buf_i, bkg_i, dxpl, forward);
+  default:
+    return -2;
+  }
+
+  return 0;
+}
+
+static herr_t enum2int(hid_t src_id,
+		       hid_t dst_id,
+		       H5T_cdata_t *cdata,
+		       size_t nl,
+		       size_t buf_stride,
+		       size_t bkg_stride,
+		       void *buf_i,
+		       void *bkg_i,
+		       hid_t dxpl) {
+  return enum_int_converter(src_id, dst_id, cdata, nl, buf_stride, bkg_stride,
+			    buf_i, bkg_i, dxpl, 1);
+}
+
+static herr_t int2enum(hid_t src_id,
+		       hid_t dst_id,
+		       H5T_cdata_t *cdata,
+		       size_t nl,
+		       size_t buf_stride,
+		       size_t bkg_stride,
+		       void *buf_i,
+		       void *bkg_i,
+		       hid_t dxpl) {
+    return enum_int_converter(src_id, dst_id, cdata, nl, buf_stride, bkg_stride,
+			      buf_i, bkg_i, dxpl, 0);
+}
 
 int
 main (void)
@@ -11,6 +176,10 @@ main (void)
     hsize_t     dims[1] = {1};
     int         ndims,
                 i, j;
+
+    hid_t tenum = H5Tenum_create(H5T_STD_I32LE);
+    H5Tregister(H5T_PERS_SOFT, "enum2int", tenum, H5T_STD_I32LE, enum2int);
+    H5Tregister(H5T_PERS_SOFT, "int2enum", H5T_STD_I32LE, tenum, int2enum);
 
     file = H5Fcreate ("h5enum.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
@@ -28,8 +197,17 @@ main (void)
     status = H5Tenum_insert (memtype, "TRUE", &tb);
     status = H5Tenum_insert (memtype, "FALSE", &fb);
 
+    fprintf(stderr, "-- bool -- \n");
     bool b = true;
     status = H5Dwrite (dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, &b);
+
+    fprintf(stderr, "-- int -- \n");
+    int foo = 0;
+    status = H5Dwrite (dset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, &foo);
+
+    fprintf(stderr, "-- int8 -- \n");
+    int8_t bar = 1;
+    status = H5Dwrite (dset, H5T_NATIVE_INT8, H5S_ALL, H5S_ALL, H5P_DEFAULT, &bar);
 
     status = H5Dclose (dset);
     status = H5Sclose (space);
